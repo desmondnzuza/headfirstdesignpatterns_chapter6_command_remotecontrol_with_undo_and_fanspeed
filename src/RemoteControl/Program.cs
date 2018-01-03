3a3c1fb16187ca04367e93675fbb1125a392f0ab
desmondnzuza <desmondnzuza@gmail.com>
2018-01-03 07:07:24 +0200

    adds initial implementation of the command pattern with fan speed implementation

---
+++ src/RemoteControl/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using RemoteControl.Appliances;
+using RemoteControl.Commands;
+using RemoteControl.Controllers;
+
+namespace RemoteControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var remoteControl = new RemoteControlWithUndo();
+
+            var livingRoonLight = new Light("Living Room");
+
+            var livingRoomLightOn = new LightOnCommand(livingRoonLight);
+            var livingRoomLightOff = new LightOffCommand(livingRoonLight);
+
+            remoteControl.SetCommand(0, livingRoomLightOn, livingRoomLightOff);
+
+            remoteControl.OnButtonWasPressed(0);
+            remoteControl.OffButtonWasPressed(0);
+            Console.WriteLine(remoteControl);
+            remoteControl.UndoButtonWasPressed();
+            remoteControl.OffButtonWasPressed(0);
+            remoteControl.OnButtonWasPressed(0);
+            Console.WriteLine(remoteControl);
+            remoteControl.UndoButtonWasPressed();
+
+            Console.ReadLine();
+        }
+    }
+}
